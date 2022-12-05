2c3f6f82e3c4ca9e9beee7018f8e4ea0cf516591
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-05 19:33:42 +0600

    replace token abi and address

--- frontend/script.js
+++ frontend/script.js
@@ -3,8 +3,23 @@ const { ethers } = require('hardhat');
 const provider = new ethers.provider.Web3Provider(window.ethereum);
 let signer;
 
-const tokenAbi = [];
-const tokenAddress = "";
+const tokenAbi = [
+  "constructor(uint256 initialSupply)",
+  "event Approval(address indexed owner, address indexed spender, uint256 value)",
+  "event Transfer(address indexed from, address indexed to, uint256 value)",
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function approve(address spender, uint256 amount) returns (bool)",
+  "function balanceOf(address account) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function decreaseAllowance(address spender, uint256 subtractedValue) returns (bool)",
+  "function increaseAllowance(address spender, uint256 addedValue) returns (bool)",
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function totalSupply() view returns (uint256)",
+  "function transfer(address to, uint256 amount) returns (bool)",
+  "function transferFrom(address from, address to, uint256 amount) returns (bool)"
+];
+const tokenAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 let tokenContract = null;
 
 const dexAbi = [
@@ -17,7 +32,7 @@ const dexAbi = [
   "function withdrawFunds()",
   "function withdrawTokens()"
 ];
-const dexAddress = "";
+const dexAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 let dexContract = null;
 
 async function getAccess() {
