abc9d940100c46e95ff3a3660a3ae75fa8777429
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-05 12:38:02 +0600

    create test file and also create descript and before functionality

---
+++ test/Token.js
@@ -0,0 +1,17 @@
+const { expect } = require("chai");
+
+describe('Token', () => {
+  let tokenSupply = '100';
+  let token;
+  let owner;
+  let addr1;
+  let addr2;
+
+  before(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory('Token');
+    token = await Token.deploy(tokenSupply);
+  });
+
+  
+})
